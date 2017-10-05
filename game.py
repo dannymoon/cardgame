91947fdd0dcda6f73c81d2db09caed0be8a0961c
dannymoon <moon.hojik@gmail.com>
2017-10-05 15:23:42 -0700

    first

---
+++ game.py
@@ -0,0 +1,11 @@
+from random import shuffle
+class Card(object):
+    def __init__(self):
+        
+class Deck(object):
+    def __init__(self):
+        self.numcards = []
+    def shufflecards(self):
+        shuffle(self.numcards)
+    def Create_Deck(self):
+        
